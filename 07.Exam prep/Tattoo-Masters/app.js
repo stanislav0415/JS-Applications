ab305e8bc63aa7fc0a62b5e696fdc368771c2bac
stanislav0415 <149686416+stanislav0415@users.noreply.github.com>
2024-11-28 07:29:00 +0200

    exam prep work in progress!

---
+++ 07.Exam prep/Tattoo-Masters/app.js
@@ -0,0 +1,38 @@
+import page from '/node_modules/page/page.mjs'
+
+
+import { myHomeView } from './view/homeView.js'
+import { myLoginView } from './view/loginView.js'
+import { myRegisterView } from './view/registerView.js'
+import { myColectionView } from './view/colectionView.js'
+import { mylogoutView } from './view/logoutView.js'
+
+
+
+
+export function updateNavi(){
+    const guest = document.getElementById('guest')
+    const user = document.getElementById('user')
+    const userData = JSON.parse(sessionStorage.getItem('userData'))
+    if(userData){
+        user.style.display = "inline-block"
+        guest.style.display = 'none' 
+    }else{
+        user.style.display = "none"
+        guest.style.display = 'inline-block' 
+    }
+}
+
+
+updateNavi()
+
+page('/', myHomeView)
+page('/login', myLoginView)
+page('/register', myRegisterView)
+page('/colection', myColectionView)
+page('/logout',mylogoutView)
+
+
+
+page.start()
+
---
+++ 07.Exam prep/Tattoo-Masters/view/loginView.js
@@ -0,0 +1,57 @@
+import { html, render } from '../node_modules/lit-html/lit-html.js'
+import { post } from '../api.js'
+import { updateNavi } from '../app.js';
+
+const loginViewTemplate = () => html`
+ <section id="login">
+          <div class="form">
+            <h2>Login</h2>
+            <form @submit="${onSubmit}" class="login-form">
+              <input type="text" name="email" id="email" placeholder="email" />
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="password"
+              />
+              <button type="submit">login</button>
+              <p class="message">
+                Not registered? <a href="#">Create an account</a>
+              </p>
+            </form>
+          </div>
+        </section>
+`
+async function onSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    if(email == '' || password == '') {
+        alert('Please fill all fields!')
+        throw new Error('Some fields is requite')
+     }
+
+    const data = await post('/users/login', {email, password});
+
+    
+        const userData = {
+            id: data._id,
+            email: data.email,
+            accessToken: data.accessToken,
+        }
+        sessionStorage.setItem('userData',JSON.stringify(userData))
+        updateNavi()
+        page.redirect('/')
+        e.target.reset()    
+}
+
+
+export async function myLoginView() {
+    render(loginViewTemplate(), document.getElementById('main'))
+}
+
+
+
+
+
